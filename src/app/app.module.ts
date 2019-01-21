26cfa7a7e36fd1e9dc01d2d4b9e483c0af6a1979
Marco Rosetti <marco.rosetti@teachat.it>
2019-01-21 17:47:52 +0100

    Added material examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { MatButtonModule, MatToolbarModule, MatCardModule, MatListModule, MatFormFieldModule, MatInputModule } from "@angular/material";
+
+import { AddProdottoComponent } from "./add-prodotto/add-prodotto.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ProdottoComponent } from "./prodotto/prodotto.component";
 import { ListaProdottiComponent } from "./lista-prodotti/lista-prodotti.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AddProdottoComponent } from "./add-prodotto/add-prodotto.component";
-import { TruncatePipe } from './truncate.pipe';
+import { ProdottoComponent } from "./prodotto/prodotto.component";
+import { TruncatePipe } from "./truncate.pipe";
 
 @NgModule({
   declarations: [AppComponent, ProdottoComponent, ListaProdottiComponent, AddProdottoComponent, TruncatePipe],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule, MatCardModule, MatListModule, MatFormFieldModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
